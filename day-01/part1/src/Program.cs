94ff9c470e49b8947916a9ae15535f4bd4ade797
William de Witt <William.DeWitt@derivco.co.za>
2021-12-21 10:30:34 +0200

    day 1 completed

---
+++ day-01/part1/src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace src
+{
+  class Program
+  {
+    private static readonly int[] Report = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+    static void Main(string[] args)
+    {
+      var increasedCount = 0;
+
+      var data = File.ReadAllLines("../../data.txt").Select(x => int.Parse(x)).ToArray();
+
+      for (int i = 0; i < data.Count(); i++)
+      {
+        if (i == 0)
+        {
+          System.Console.WriteLine($"{data[i]} (N/A - no previous measurement)");
+        }
+        else if (data[i] > data[i - 1])
+        {
+          System.Console.WriteLine($"{data[i]} (increased)");
+          increasedCount++;
+        }
+        else if (data[i] < data[i - 1])
+        {
+          System.Console.WriteLine($"{data[i]} (decreased)");
+        }
+      }
+
+      System.Console.WriteLine($"The depth has increased {increasedCount} times.");
+    }
+  }
+}
---
+++ day-01/part2/src/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace src
+{
+  class Program
+  {
+    private static readonly int[] Report = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+    static void Main(string[] args)
+    {
+      var increasedCount = 0;
+
+      var data = File.ReadAllLines("../../data.txt").Select(x => int.Parse(x)).ToList();
+
+      var threes = new List<int>();
+      
+      for (int i = 0; i < data.Count() -2; i++)
+      {
+        var val = data.Skip(i).Take(3).Sum();
+        threes.Add(val);
+      }
+
+      for (int i = 0; i < threes.Count(); i++)
+      {
+        if (i == 0)
+        {
+          System.Console.WriteLine($"{threes[i]} (N/A - no previous measurement)");
+        }
+        else if (threes[i] == threes[i - 1])
+        {
+          System.Console.WriteLine($"{threes[i]} (no change)");
+        }
+        else if (threes[i] > threes[i - 1])
+        {
+          System.Console.WriteLine($"{threes[i]} (increased)");
+          increasedCount++;
+        }
+        else if (threes[i] < threes[i - 1])
+        {
+          System.Console.WriteLine($"{threes[i]} (decreased)");
+        }
+      }
+
+      System.Console.WriteLine($"The depth has increased {increasedCount} times.");
+    }
+  }
+}
