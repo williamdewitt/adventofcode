344997b96917e6bf03a924f1a42de15db49099ac
William de Witt <William.DeWitt@derivco.co.za>
2021-12-23 17:40:38 +0200

    comple anser for day 3 part1

--- day-03/part1/src/Program.cs
+++ day-03/part1/src/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
+
 namespace src
 {
   class Program
@@ -22,6 +23,23 @@ namespace src
       "01010"
       };
 
+      private static readonly IList<byte> _testDataAsBytes = new List<byte>(){
+      0b00100,
+      0b11110,
+      0b10110,
+      0b10111,
+      0b10101,
+      0b01111,
+      0b00111,
+      0b11100,
+      0b10000,
+      0b11001,
+      0b00010,
+      0b01010
+      };
+
+      //0b00100
+
     static void Main(string[] args)
     {
       var gammaRate = "";
@@ -38,41 +56,41 @@ namespace src
         if (value > threshold)
         {
           gammaRate += "1";
+          epsilonRate += "0";
         }
         else
         {
           gammaRate += "0";
+          epsilonRate += "1";
         }
       }
 
-      for (int i = 0; i < puzzleData.First().Length; i++)
-      {
-        var value = puzzleData.Select(x => char.GetNumericValue(x[i])).Sum();
+      var gammaRateAsDecimal = BinaryStringToDecimal(gammaRate);
+      var epsilonRateAsDecimal = BinaryStringToDecimal(epsilonRate);
+      
+      System.Console.WriteLine($"And the answer is... {gammaRateAsDecimal * epsilonRateAsDecimal}");
+    }
 
-        if (value < threshold)
+    private static double BinaryStringToDecimal(string binaryString)
+    {
+      double myDecimal = 0;
+      int power = 0;
+
+      for (int i = binaryString.Length -1; i >= 0; i--)
+      {
+        if(binaryString[i] == '0')
         {
-          epsilonRate += "1";
+          myDecimal += System.Math.Pow(2, power) * 0;
         }
         else
         {
-          epsilonRate += "0";
+          myDecimal += System.Math.Pow(2, power) * 1;
         }
+
+        power++;
       }
-
-      //var bytes = gammaRate.Select(x => (byte)x).ToArray();
-      //var chars = System.Text.Encoding.UTF8.GetString(bytes).ToCharArray();
-
-      // TODO: This section needs work
-
-      System.Console.WriteLine(gammaRate);
-      System.Console.WriteLine(epsilonRate);
-
-      // replace with cw output from above lines
-      System.Console.WriteLine(0b110111000111);
-      System.Console.WriteLine(0b001000111000);
-
-      // multiply output to get the answer
-      System.Console.WriteLine($"And the answer is... {3527 * 568}");
+      
+      return myDecimal;
     }
   }
 }
