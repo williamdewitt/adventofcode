ab9247419e3223c1e8c5ff68e74a6db6bf54916b
William de Witt <William.DeWitt@derivco.co.za>
2021-12-23 20:25:51 +0200

    D3P1 cleans up BinaryStringToDouble function

--- day-03/part1/src/Program.cs
+++ day-03/part1/src/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-
+using System.IO;
 
 namespace src
 {
   class Program
   {
-    private static readonly IList<string> _testData = new List<string>(){
+    private static readonly IList<string> _sampleData = new List<string>(){
       "00100",
       "11110",
       "10110",
@@ -23,30 +22,12 @@ namespace src
       "01010"
       };
 
-      private static readonly IList<byte> _testDataAsBytes = new List<byte>(){
-      0b00100,
-      0b11110,
-      0b10110,
-      0b10111,
-      0b10101,
-      0b01111,
-      0b00111,
-      0b11100,
-      0b10000,
-      0b11001,
-      0b00010,
-      0b01010
-      };
-
-      //0b00100
-
     static void Main(string[] args)
     {
       var gammaRate = "";
       var epsilonRate = "";
 
-      var puzzleData = System.IO.File.ReadAllLines("../../data.txt");
-
+      var puzzleData = File.ReadAllLines("../../data.txt");
       var threshold = puzzleData.Count() / 2;
 
       for (int i = 0; i < puzzleData.First().Length; i++)
@@ -65,32 +46,19 @@ namespace src
         }
       }
 
-      var gammaRateAsDecimal = BinaryStringToDecimal(gammaRate);
-      var epsilonRateAsDecimal = BinaryStringToDecimal(epsilonRate);
-      
-      System.Console.WriteLine($"And the answer is... {gammaRateAsDecimal * epsilonRateAsDecimal}");
+      var gammaRateAsDouble = BinaryStringToDouble(gammaRate);
+      var epsilonRateAsDouble = BinaryStringToDouble(epsilonRate);
+      var powerConsumption = gammaRateAsDouble * epsilonRateAsDouble;
+
+      Console.WriteLine($"The power consumption is... {powerConsumption}");
     }
 
-    private static double BinaryStringToDecimal(string binaryString)
+    private static double BinaryStringToDouble(string binaryString)
     {
-      double myDecimal = 0;
-      int power = 0;
-
-      for (int i = binaryString.Length -1; i >= 0; i--)
-      {
-        if(binaryString[i] == '0')
-        {
-          myDecimal += System.Math.Pow(2, power) * 0;
-        }
-        else
-        {
-          myDecimal += System.Math.Pow(2, power) * 1;
-        }
-
-        power++;
-      }
-      
-      return myDecimal;
+      return binaryString
+        .Reverse()
+        .Select((item, index) => Math.Pow(2, index) * char.GetNumericValue(item))
+        .Sum();
     }
   }
 }
